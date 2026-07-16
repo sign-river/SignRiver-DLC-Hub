e26a1f3d542355b2d444dcaf15511cf3392bb977
sign-river <3217344726@qq.com>
2026-07-17 01:09:41 +0800

    Remove obsolete files and streamline the codebase

--- .pytest-tmp-catalog-tools-20260712/test_loads_external_applicatio0/app/versions/0.1.0/entry.py
+++
@@ -1,6 +0,0 @@
-from . import helper
-class App:
-    def run(self):
-        self.value = helper.VALUE
-def create_application(context):
-    return App()
--- .pytest-tmp-update-chain-final-20260712/test_installs_to_new_version_a0/app/versions/0.1.1/app_entry.py
+++
@@ -1,3 +0,0 @@
-class App:
-    def run(self): pass
-def create_application(context): return App()
